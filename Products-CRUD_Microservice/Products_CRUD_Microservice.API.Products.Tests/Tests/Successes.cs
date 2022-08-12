6c88402d1b19a16dd0422929f45f0f44f935ca8e
kgomez <kevin.gomez.codina@outlook.es>
2022-08-12 07:54:53 +0200

    - Creación de todas las pruebas unitarias exitosas.

--- Products-CRUD_Microservice/Products_CRUD_Microservice.API.Products.Tests/Tests/Successes.cs
+++ Products-CRUD_Microservice/Products_CRUD_Microservice.API.Products.Tests/Tests/Successes.cs
@@ -1,6 +1,5 @@
 using Products_CRUD_Microservice.API.Models;
 using Products_CRUD_Microservice.Models.Products.DTO;
-using Products_CRUD_Microservice.Utils;
 
 namespace Products_CRUD_Microservice.API.Products.Tests.Tests
 {
@@ -9,9 +8,89 @@ namespace Products_CRUD_Microservice.API.Products.Tests.Tests
         [Test]
         public void GetById()
         {
-            ApiResponse<ProductDTO> response = new RestClient<ApiResponse<ProductDTO>>("https://localhost:7046/api/v1.0/products/getById?id=1")
-                .AddHeader("Accept", "application/json")
-                .Execute();
+            ApiResponse<ProductDTO> response = base.GetProductById(1);
+
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Data);
+            Assert.IsNull(response.Errors);
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void GetByName()
+        {
+            ApiResponse<ProductDTO> response = base.GetProductByName("Prueba1");
+
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Data);
+            Assert.IsNull(response.Errors);
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void Create()
+        {
+            ProductDTO productDTO = new ProductDTO()
+            {
+                Name = "Prueba666",
+                Description = "Descripción 666",
+                Price = 666.69,
+                Enabled = true,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+            ApiResponse<ProductDTO> response = base.CreateProduct(productDTO);
+
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Data);
+            Assert.IsNull(response.Errors);
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+            Assert.That(response.Data.Id, Is.AtLeast(1));
+        }
+
+        [Test]
+        public void Update()
+        {
+            int productId = 3002;
+
+            #region Obtain the product.
+
+            ApiResponse<ProductDTO> response = base.GetProductById(productId);
+
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Data);
+            Assert.IsNull(response.Errors);
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+
+            #endregion
+
+            #region Update the product.
+
+            response.Data.Name = "Prueba777";
+            response.Data.Description = "Descripción 777";
+            response.Data.Price = 777.666;
+            response.Data.Enabled = false;
+            response.Data.UpdatedAt = DateTime.Now;
+
+            response = base.UpdateProduct(response.Data);
+
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Data);
+            Assert.IsNull(response.Errors);
+            Assert.That(response.StatusCode, Is.EqualTo(200));
+            Assert.That(response.Data.Id, Is.EqualTo(productId));
+            Assert.That(response.Data.Name, Is.EqualTo("Prueba777"));
+            Assert.That(response.Data.Description, Is.EqualTo("Descripción 777"));
+            Assert.That(response.Data.Price, Is.EqualTo(777.666));
+            Assert.That(response.Data.Enabled, Is.EqualTo(false));
+
+            #endregion
+        }
+
+        [Test]
+        public void Delete()
+        {
+            ApiResponse<string> response = base.DeleteProduct(3002);
 
             Assert.IsNotNull(response);
             Assert.IsNotNull(response.Data);
--- Products-CRUD_Microservice/Products_CRUD_Microservice.API.Products.Tests/UnitTest.cs
+++ Products-CRUD_Microservice/Products_CRUD_Microservice.API.Products.Tests/UnitTest.cs
@@ -1,4 +1,8 @@
-﻿namespace Products_CRUD_Microservice.API.Products.Tests
+﻿using Products_CRUD_Microservice.API.Models;
+using Products_CRUD_Microservice.Models.Products.DTO;
+using Products_CRUD_Microservice.Utils;
+
+namespace Products_CRUD_Microservice.API.Products.Tests
 {
     public class UnitTest
     {
@@ -6,6 +10,41 @@
         public void Setup()
         {
             // NOTE: Esta función siempre se ejecuta antes de ejecutar un Test.
+        }
+
+        protected virtual ApiResponse<ProductDTO> GetProductById(int id)
+        {
+            return new RestClient<ApiResponse<ProductDTO>>(String.Format("https://localhost:7046/api/v1.0/products/getById?id={0}", id))
+                .AddHeader("Accept", "application/json")
+                .Execute();
+        }
+
+        protected virtual ApiResponse<ProductDTO> GetProductByName(string name)
+        {
+            return new RestClient<ApiResponse<ProductDTO>>(String.Format("https://localhost:7046/api/v1.0/products/getByName?name={0}", name))
+                .AddHeader("Accept", "application/json")
+                .Execute();
+        }
+
+        protected virtual ApiResponse<ProductDTO> CreateProduct(ProductDTO productDTO)
+        {
+            return new RestClient<ApiResponse<ProductDTO>>("https://localhost:7046/api/v1.0/products/create", HttpMethod.Post)
+                .AddHeader("Accept", "application/json")
+                .Execute(productDTO);
+        }
+
+        protected virtual ApiResponse<ProductDTO> UpdateProduct(ProductDTO productDTO)
+        {
+            return new RestClient<ApiResponse<ProductDTO>>("https://localhost:7046/api/v1.0/products/update", HttpMethod.Put)
+                .AddHeader("Accept", "application/json")
+                .Execute(productDTO);
+        }
+
+        protected virtual ApiResponse<string> DeleteProduct(int id)
+        {
+            return new RestClient<ApiResponse<string>>(String.Format("https://localhost:7046/api/v1.0/products/delete?id={0}", id), HttpMethod.Delete)
+                .AddHeader("Accept", "application/json")
+                .Execute();
         }
     }
 }
